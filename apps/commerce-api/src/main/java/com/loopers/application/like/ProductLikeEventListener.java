7b0404c814d9767623fe979e0878e5c6e1895e46
HongChangMo <hcm7402@naver.com>
2025-12-11 17:37:27 +0900

    refactor: 좋아요 - 집계 이벤트 트랜잭션 phase, propagation 수정
    - 좋아요 집계 처리에 관계 없이 좋아요 정상 처리 목적
    - phase : AFTER_COMMIT 변경
    - propagation : REQUIRES_NEW 변경

--- apps/commerce-api/src/main/java/com/loopers/application/like/ProductLikeEventListener.java
+++ apps/commerce-api/src/main/java/com/loopers/application/like/ProductLikeEventListener.java
@@ -8,6 +8,7 @@ import com.loopers.domain.product.Product;
 import com.loopers.domain.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,10 +25,11 @@ public class ProductLikeEventListener {
 
     /**
      * 좋아요 추가 시 집계 처리
-     * - 트랜잭션 커밋 전 실행 (같은 트랜잭션) 동기 처리
+     * - 집계 이벤트는 별도 트랜잭션으로 처리( 집계 로직의 성공/실패와 상관 없이, 좋아요 처리는 정상적으로 완료되어야함 )
      */
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
-    @Transactional(propagation = Propagation.MANDATORY)
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleProductLikeAdded(ProductLikeAddedEvent event) {
         log.info("좋아요 추가 집계 처리 이벤트 시작 - ProductId: {}", event.getProductId());
 
@@ -43,10 +45,11 @@ public class ProductLikeEventListener {
 
     /**
      * 좋아요 취소 시 집계 처리
-     * - 트랜잭션 커밋 전 실행 (같은 트랜잭션) 동기 처리
+     * - 집계 이벤트는 별도 트랜잭션으로 처리( 집계 로직의 성공/실패와 상관 없이, 좋아요 처리는 정상적으로 완료되어야함 )
      * */
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
-    @Transactional(propagation = Propagation.MANDATORY)
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleProductLikeRemoved(ProductLikeRemovedEvent event) {
         log.info("좋아요 취소 집계 처리 시작 - ProductId: {}", event.getProductId());
 
@@ -54,6 +57,7 @@ public class ProductLikeEventListener {
         ProductLike like = productLikeService.getProductLikeById(event.getLikeId());
 
         product.decrementLikeCount(like);
+
         log.info("좋아요 취소 집계 처리 이벤트 완료 - ProductId: {}, 현재 좋아요 수: {}",
                 event.getProductId(), product.getLikeCount());
     }
